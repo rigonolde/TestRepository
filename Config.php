e7cdeb4534f5561a5b2368829e4093f30d209307
rigonolde <j.zafimananga@matelli.mg>
2017-08-30 12:07:23 +0300

    mise en place du projet

---
+++ Config.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Config
+ *
+ * @author josio
+ */
+class Config
+{
+    public $BD_NAME = '';
+    public $BD_PORT = '';
+    public $BD_HOST = '';
+    public $BD_USER = '';
+    public $BD_PASSWD = '';
+
+}
