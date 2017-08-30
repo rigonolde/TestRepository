edaa0f50b36d29419ab2b3af5f28b2e1cb315c0d
rigonolde <j.zafimananga@matelli.mg>
2017-08-30 16:03:30 +0300

    mise en place du projet

---
+++ Manager/DB.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Manager;
+
+require_once('../Config/Config.php');
+
+/**
+ * Description of DBConnexion
+ *
+ * @author josio
+ */
+class DB
+{
+    private $config;
+    private $statement;
+
+    public function __construct()
+    {
+        $this->config = new \Config\Config();
+        $this->statement = null;
+        $this->bdConnexion();
+    }
+
+    public function bdConnexion()
+    {
+        $conn = new \mysqli($this->config->BD_HOST, $this->config->BD_USER, $this->config->BD_PASSWD, $this->config->BD_NAME, $this->config->BD_PORT);
+
+        if ($conn->connect_error) {
+            return false;
+        }
+        $this->setStatement($conn);
+        return true;
+    }
+
+    function getConfig()
+    {
+        return $this->config;
+    }
+
+    function getStatement()
+    {
+        return $this->statement;
+    }
+
+    function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+    function setStatement($statement)
+    {
+        $this->statement = $statement;
+    }
+
+}
